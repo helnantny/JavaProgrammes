02b5cb62a2759efafa03dac3ba187ecbe47cb820
helnantny <helnantnyc@gmail.com>
2024-09-07 12:08:16 +0530

    Create area.java

---
+++ area.java
@@ -0,0 +1,12 @@
+import java.util.Scanner
+public class Area
+{
+public static void main(Stringargs[])
+{
+Scanner sc=new Scanner(System.in)
+System.out.println("enter length and breadth");
+int l=sc.nextInt();
+int b=sc.nextInt();
+int a=l*b;
+System.out.println("area="+a);
+}}
